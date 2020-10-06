0f67c5729230daab7d0a04567f277c9be979e227
Atsuhito Kita <kitaa@oregonstate.edu>
2020-10-06 01:30:20 -0700

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 9900;
+//var express = require('express');
+//var app = express();
+//var port = process.env.PORT || 9900;
+//app.listen(port, function(){
+//    console.log("== Server listening on port", port);
+//});
 
-app.listen(port, function(){
-    console.log("== Server listening on port", port);
-});
+var http = require('http')
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {"Content-Type": "text/plain"})
+	res.end("Hello world\n")
+}).listen(process.env.PORT)
